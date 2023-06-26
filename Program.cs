3970effdc75775d4b676d4f167d6f602fe81918b
sanjana8100 <sanjanams812@gmail.com>
2023-06-26 22:25:35 +0530

    Data Structure: Collections

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DND16_DataStructureCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
